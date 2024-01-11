0cf94e3d75a8ae95735181447292a3dacc22150b
uchuvatova <109352814+uchuvatova@users.noreply.github.com>
2024-01-11 10:33:33 +0300

    Добавлен тест про ошибку при регистрации с длинным паролем

--- ui_tests/test_registration.py
+++ ui_tests/test_registration.py
@@ -101,32 +101,32 @@ class TestRegistration:
         with allure.step("Кнопка Профиль не появляется"):
             main_page.should_be_not_visible_button_profile()
 
-        @allure.title("Регистрация с длинным паролем")
-        def test_registration_with_different_password(self, browser_setup):
-            email = random_char(7) + "@rover.info"
-            password = random_char(101)
-            main_page = MainPage()
-            with allure.step("Открыть главную страницу"):
-                main_page.open()
-            # WHEN
-            with allure.step("Нажать кнопку Войти в навбаре"):
-                main_page.click_login_on_navbar()
-            with allure.step("Заполнить email на модальном окне Войти"):
-                main_page.fill_email(email)
-            with allure.step("Нажать на Продолжить на модальном окне Войти"):
-                main_page.click_continue()
-            with allure.step("Отметить чекбокс с согласием на модальном окне Создать профиль"):
-                main_page.checkbox_agree_on_modal()
-            with allure.step("Нажать на Продолжить на модальном окне Создать профиль"):
-                main_page.click_continue_after_email()
-            with allure.step("Заполнить пароль"):
-                main_page.fill_password(password)
-            with allure.step("Повторить пароль"):
-                main_page.repeat_password(password)
-            with allure.step("Нажать Сохранить на модальном окне Задать пароль"):
-                main_page.click_enter_on_modal()
-            # THEN
-            with allure.step("Появление предупреждения, что пароль слишком длинный"):
-                main_page.should_be_error_long_password()
-            with allure.step("Кнопка Профиль не появляется"):
-                main_page.should_be_not_visible_button_profile()
+    @allure.title("Регистрация с длинным паролем")
+    def test_registration_with_different_password(self, browser_setup):
+        email = random_char(7) + "@rover.info"
+        password = random_char(101)
+        main_page = MainPage()
+        with allure.step("Открыть главную страницу"):
+            main_page.open()
+        # WHEN
+        with allure.step("Нажать кнопку Войти в навбаре"):
+            main_page.click_login_on_navbar()
+        with allure.step("Заполнить email на модальном окне Войти"):
+            main_page.fill_email(email)
+        with allure.step("Нажать на Продолжить на модальном окне Войти"):
+            main_page.click_continue()
+        with allure.step("Отметить чекбокс с согласием на модальном окне Создать профиль"):
+            main_page.checkbox_agree_on_modal()
+        with allure.step("Нажать на Продолжить на модальном окне Создать профиль"):
+            main_page.click_continue_after_email()
+        with allure.step("Заполнить пароль"):
+            main_page.fill_password(password)
+        with allure.step("Повторить пароль"):
+            main_page.repeat_password(password)
+        with allure.step("Нажать Сохранить на модальном окне Задать пароль"):
+            main_page.click_enter_on_modal()
+        # THEN
+        with allure.step("Появление предупреждения, что пароль слишком длинный"):
+            main_page.should_be_error_long_password()
+        with allure.step("Кнопка Профиль не появляется"):
+            main_page.should_be_not_visible_button_profile()
